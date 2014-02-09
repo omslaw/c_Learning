675b5605c76e83363c3259b6a1cd4ef276cf1bdc
omslaw <cafeconleche@overhere.com>
2014-02-08 19:43:32 -0800

    quick commit

--- cs106b_testingArea/main.cpp
+++ cs106b_testingArea/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Finally got this to work from QT creator, but utimately, will just use Git shell." << endl;
+    cout << "Fucking pain in the ass!!!" << endl;
     return 0;
 }
 
